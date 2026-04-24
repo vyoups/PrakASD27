20b591b0921db0015bcc4defb18565bc31e589b2
Rifat <marcianorifat5@gmail.com>
2026-04-24 08:50:44 +0700

    pertemuan 9 praktikum 1 pertanyaan 4

---
+++ src/P9/MahasiswaDemo27.java
@@ -0,0 +1,60 @@
+package P9;
+import java.util.Scanner;
+
+public class MahasiswaDemo27 {
+    public static void main(String[] args) {
+        StackTugasMahasiswa27 stack = new StackTugasMahasiswa27(5);
+        Scanner sc = new Scanner(System.in);
+        int pilih;
+
+        do {
+            System.out.println("\nMenu:");
+            System.out.println("1. Mengumpulkan tugas");
+            System.out.println("2. Menilai tugas");
+            System.out.println("3. Melihat tugas teratas");
+            System.out.println("4. Melihat Daftar Tugas");
+            System.out.print("Pilih: ");
+            pilih = sc.nextInt();
+            sc.nextLine();
+
+            switch (pilih) {
+                case 1:
+                    System.out.print("Nama: ");
+                    String nama = sc.nextLine();
+                    System.out.print("NIM: ");
+                    String nim = sc.nextLine();
+                    System.out.print("Kelas: ");
+                    String kelas = sc.nextLine();
+                    Mahasiswa27 mhs = new Mahasiswa27(nama, nim, kelas);
+                    stack.push(mhs);
+                    System.out.printf("Tugas %s berhasil dikumpulkan!", mhs.nama);
+                    break;
+                case 2:
+                    Mahasiswa27 dinilai = stack.pop();
+                    if (dinilai != null) {
+                        System.out.println("Menilai tugas dari " + dinilai.nama);
+                        System.out.print("Masukan nilai (0-100): ");
+                        int nilai = sc.nextInt();
+                        dinilai.tugasDinilai(nilai);
+                        System.out.printf("Nilai tugas %s adalah %d%n", dinilai.nama, dinilai.nilai);
+                    }
+                    break;
+                case 3:
+                    Mahasiswa27 lihat = stack.peek();
+                    if (lihat != null) {
+                        System.out.printf("Tugas terakhir dikumpulkan oleh: %s%n", lihat.nama);
+                    }
+                    break;
+                case 4:
+                    System.out.println("Daftar Tugas:");
+                    System.out.println("Nama\tNIM\tKelas");
+                    stack.print();
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid");
+                    break;
+            }
+        } while (pilih >= 1 && pilih <= 4); 
+        sc.close();
+    }
+}
---
+++ src/P9/report.md
@@ -0,0 +1,102 @@
+|  | Algoritma dan Struktur Data |
+|--|--|
+| NIM |  254107020238|
+| Nama |  Rifat Marciano Putera |
+| Kelas | TI - 1F |
+| Repository | [link] https://github.com/vyoups/PrakASD27
+
+# Jobsheet 9 Stack
+
+## Hasil Running Praktikum ke-1
+```Menu:
+1. Mengumpulkan tugas
+2. Menilai tugas
+3. Melihat tugas teratas
+4. Melihat Daftar Tugas
+Pilih: 1
+Nama: dudung
+NIM: 23456765432
+Kelas: TI 1F
+Tugas dudung berhasil dikumpulkan!
+Menu:
+1. Mengumpulkan tugas
+2. Menilai tugas
+3. Melihat tugas teratas
+4. Melihat Daftar Tugas
+Pilih: 1
+Nama: Erik
+NIM: 2332142536  
+Kelas: TI 1A
+Tugas Erik berhasil dikumpulkan!
+Menu:
+1. Mengumpulkan tugas
+2. Menilai tugas
+3. Melihat tugas teratas
+4. Melihat Daftar Tugas
+Pilih: 3
+Tugas terakhir dikumpulkan oleh: Erik
+
+Menu:
+1. Mengumpulkan tugas
+2. Menilai tugas
+3. Melihat tugas teratas
+4. Melihat Daftar Tugas
+Pilih: 1
+Nama: tika
+NIM: 234567891
+Kelas: TI 1C
+Tugas tika berhasil dikumpulkan!
+Menu:
+1. Mengumpulkan tugas
+2. Menilai tugas
+3. Melihat tugas teratas
+4. Melihat Daftar Tugas
+Pilih: 4
+Daftar Tugas:
+Nama    NIM     Kelas
+dudung 23456765432 TI 1F
+Erik 2332142536 TI 1A
+tika 234567891 TI 1C
+
+
+Menu:
+1. Mengumpulkan tugas
+2. Menilai tugas
+3. Melihat tugas teratas
+4. Melihat Daftar Tugas
+Pilih: 2
+Menilai tugas dari tika
+Masukan nilai (0-100): 87
+Nilai tugas tika adalah 87
+
+Menu:
+1. Mengumpulkan tugas
+2. Menilai tugas
+3. Melihat tugas teratas
+4. Melihat Daftar Tugas
+Pilih: 4
+Daftar Tugas:
+Nama    NIM     Kelas
+dudung 23456765432 TI 1F
+Erik 2332142536 TI 1A
+
+
+Menu:
+1. Mengumpulkan tugas
+2. Menilai tugas
+3. Melihat tugas teratas
+4. Melihat Daftar Tugas
+Pilih: 
+```
+## Jawaban dari pertanyaan praktikum ke-1
+1. Tambahkan method main , Posisi whilenya salah
+--- 
+2. 5 data
+```java
+StackTugasMahasiswa27 stack = new StackTugasMahasiswa27(5);
+```
+---
+3.  Untuk mencegah stack overflow. Yaitu kondisi ketika data dimasukkan melebihi kapasitas array.
+java
+---
+4. 
